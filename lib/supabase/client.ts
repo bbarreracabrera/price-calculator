5779be1f14da0ad932709ebb2812bd0ef1394eae
Benjamin Barrera <b.barreracabrera@gmail.com>
2026-01-10 04:33:49 -0300

    fix: properly typed supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,10 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr'
-import { Database } from './types'
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from './types'
 
-export const createClient = () => {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
-
+export const supabase = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
