da649e7d838b560485b2faeeec72e7bd9575aa96
Benjamin Barrera <b.barreracabrera@gmail.com>
2026-01-17 10:06:39 -0300

    Cambio para vercel y android

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+// Detectamos si estamos construyendo para móvil usando una variable de entorno
+const isMobile = process.env.NEXT_PUBLIC_IS_MOBILE === 'true';
+
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // Si es móvil, usamos 'export' (archivos estáticos). Si es web, usamos el normal (undefined)
+  output: isMobile ? 'export' : undefined,
+  
+  // En móvil no hay servidor para optimizar imágenes, así que las desoptimizamos
+  images: {
+    unoptimized: isMobile ? true : false
+  }
+};
 
-module.exports = nextConfig
-
+export default nextConfig;
